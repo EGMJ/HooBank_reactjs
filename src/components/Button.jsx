aabd78bb8f48268c92200209b888287ee9fd3a25
Odisseus <56776978+EGMJ@users.noreply.github.com>
2022-11-23 09:01:43 -0300

    hoobank project

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({ styles }) => {
+  return (
+    <button type='button' className={`${styles} py-4 px-6 bg-blue-gradient font-poppins font-medium rounded-[12px] text-[18px] text-primary outline-none`}>Get Started</button>
+  )
+}
+
+export default Button
